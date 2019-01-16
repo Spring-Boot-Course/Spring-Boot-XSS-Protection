68f9afad612e0ff43be4968166913b59b1169daf
SML <dltkdals2202@gmail.com>
2019-01-17 01:07:16 +0900

    feat : response xss escape added at objectMapper
    Signed-off-by: SML <dltkdals2202@gmail.com>

---
+++ src/main/java/me/sml/xssdemo/WebConfig.java
@@ -0,0 +1,50 @@
+package me.sml.xssdemo;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.navercorp.lucy.security.xss.servletfilter.XssEscapeServletFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    // Form data
+    @Bean
+    public FilterRegistrationBean getXssEscapeServletFilterRegistrationBean() {
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        registrationBean.setFilter(new XssEscapeServletFilter());
+        registrationBean.setOrder(1);
+        registrationBean.addUrlPatterns("/*"); //filter를 거칠 url patterns
+        return registrationBean;
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(escapingConverter());
+
+    }
+
+    @Bean
+    public HttpMessageConverter escapingConverter() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.getFactory().setCharacterEscapes(new HTMLCharacterEscapes());
+//        objectMapper.registerModule(new JavaTimeModule());
+//        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        MappingJackson2HttpMessageConverter escapingConverter =
+                new MappingJackson2HttpMessageConverter();
+        escapingConverter.setObjectMapper(objectMapper);
+
+        return escapingConverter;
+    }
+
+}
--- src/main/java/me/sml/xssdemo/restcontroller/WebRestController.java
+++ src/main/java/me/sml/xssdemo/restcontroller/WebRestController.java
@@ -1,12 +1,15 @@
 package me.sml.xssdemo.restcontroller;
 
 import lombok.RequiredArgsConstructor;
-import me.sml.xssdemo.domain.posts.PostsRepository;
+import me.sml.xssdemo.dto.posts.PostsMainResponseDto;
 import me.sml.xssdemo.dto.posts.PostsSaveRequestDto;
 import me.sml.xssdemo.service.PostsService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,5 +20,11 @@ public class WebRestController {
     @PostMapping("/post")
     public void savePosts(@RequestBody PostsSaveRequestDto dto){
         postsService.save(dto);
+    }
+
+    @GetMapping("/post")
+    public List<PostsMainResponseDto> getPosts(){
+        List<PostsMainResponseDto> results = postsService.findAllDesc();
+        return results;
     }
 }
